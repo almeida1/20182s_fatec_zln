b9426ee865bfaf80b929888501283a9767fbb34e
professor <professor@L1MP.IPIRANGA.FATEC>
2018-10-11 21:07:20 -0300

    Testes da classe usuario e livro

--- sce/src/test/java/com/fatec/sce/UC01CadastrarLivro.java
+++ sce/src/test/java/com/fatec/sce/UC01CadastrarLivro.java
@@ -23,4 +23,50 @@ public class UC01CadastrarLivro {
 		}
 	}
 
+	@Test
+	public void CT02cadastrarLivroComISBN_em_branco() {
+		// cenario
+		Livro livro = new Livro();
+		livro.setTitulo("Engenharia de Software");
+		livro.setAutor("Pressman");
+		try {
+			// acao
+			livro.setIsbn("");
+			fail("deveria lançar uma exceção");
+		} catch (RuntimeException e) {
+			// verificacao
+			assertEquals("ISBN invalido", e.getMessage());
+		}
+	}
+
+	@Test
+	public void CT03cadastrarLivroComISBN_nulo() {
+		// cenario
+		Livro livro = new Livro();
+		livro.setTitulo("Engenharia de Software");
+		livro.setAutor("Pressman");
+		try {
+			// acao
+			livro.setIsbn(null);
+			fail("deveria lançar uma exceção");
+		} catch (RuntimeException e) {
+			// verificacao
+			assertEquals("ISBN invalido", e.getMessage());
+		}
+	}
+
+	@Test
+	public void CT04CadastrarLivro_com_sucesso() {
+		// cenario
+		Livro livro = new Livro();
+		livro.setTitulo("Engenharia de Software");
+		livro.setAutor("Pressman");
+		livro.setIsbn("1111");
+		assertEquals("Engenharia de Software", livro.getTitulo());
+	}
+	
+	
+	
+	
+	
 }
---
+++ sce/src/test/java/com/fatec/sce/UC05CadastrarUsuario.java
@@ -0,0 +1,14 @@
+package com.fatec.sce;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UC05CadastrarUsuario {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
